4f8ba97e271e40df973931fb97f54e848da57434
Ben Muthalaly <benmuthalaly@gmail.com>
2025-10-23 15:03:11 -0400

    Fix collision mvp.

--- main.glsl
+++ main.glsl
@@ -1,6 +1,6 @@
 // this function happens for every pixel!
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 top_coord = vec2(fragCoord.x, fragCoord.y + 2) / iResolution.xy;
+    vec2 top_coord = vec2(fragCoord.x, fragCoord.y) / iResolution.xy;
     vec4 top_color = texture(iChannel0, top_coord);
 
     // sample a pixel from ghostty's pixel buffer
@@ -9,18 +9,16 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // sometimes nothing happens, something the whole window changes to a solid color
     fragColor = pixel_color;
 
-    vec4 magenta = vec4(1.0, 0.0, 1.0, 1.0);
-
-    if (fragCoord.y > 400 && fragCoord.y < 401 && (fragCoord.x / iResolution.x < ((iFrame % 800) / 800.))) {
-        // check for collision with the cursor to the right
-        if (length(top_color - magenta) < 0.1) {
-            fragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    // helix's cursor isn't rendering as pure #FF00FFFF, so we try to match the actual shade here
+    vec4 magenta = vec4(0.917, 0.2, 0.968, 1.0);
+    if (fragCoord.y > 400 && fragCoord.y < 410 && (fragCoord.x / iResolution.x < ((iFrame % 800) / 800.))) {
+        if (distance(top_color, magenta) < 0.1) {
+            fragColor = vec4(0.0, 0.0, 0.0, 1.0);
         } else {
             fragColor = vec4(1.0, 1.0, 0.0, 1.0);
         }
-
-        if (fragCoord.x < 20. && fragCoord.y < 20.) {
-            fragColor = vec4(0.0, 0.0, 1.0, 1.0);
-        }
+    }
+    if (fragCoord.x < 20. && fragCoord.y < 20.) {
+        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }
 }
